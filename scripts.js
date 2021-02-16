6518854e0ae6fd33b61a70a82cd555bc641d9c98
Sebi <77812917+BSebi@users.noreply.github.com>
2021-02-17 00:36:03 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var button = document.queerySelector(".button");
+var image =  document.queerySelector(".home-page-image");
+
+button.addEventListener("click", function()   {
+   image.style.width = "400px";
+},  false);
